5dc9a09071e6d793f2771260d92d4dd7aa230f90
Dave Wolf <dave45678@gmail.com>
2016-05-14 00:05:15 -0400

    added JPA to Oracle 11g and SQL Scripts

---
+++ src/TestServlet.java
@@ -0,0 +1,52 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.persistence.EntityManager;
+import customTools.DBUtil;
+
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EntityManager em = DBUtil.getEmFactory().createEntityManager();
+		try {
+			model.Bhuser user = em.find(model.Bhuser.class, (long)1);
+			System.out.println(user.getUsername());
+		} catch (Exception e){
+			System.out.println(e);
+		} finally {
+			em.close();
+			System.out.println("cerrado!");
+		}
+
+	}
+
+}
